634a00c3a3f6e524c92bf51189a9a26cb28b8f65
Renato <52421382+deus-magna@users.noreply.github.com>
2021-04-06 20:01:51 -0600

    Adding Hive for local storage for pokedex entries

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,9 +1,12 @@
 import 'package:data_connection_checker/data_connection_checker.dart';
 import 'package:get_it/get_it.dart';
 import 'package:http/http.dart' as http;
+import 'package:rotom_phone/data/datasource/pokemon/pokemon_local_datasource.dart';
 import 'package:rotom_phone/domain/repositories/pokemon_repository.dart';
 import 'package:rotom_phone/domain/usercases/pokemon/get_paginated_pokemon_list.dart';
 import 'package:rotom_phone/presentation/cubit/pokedex/pokedex_cubit.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 import 'core/network/network_info.dart';
 import 'data/datasource/pokemon/pokemon_remote_datasource.dart';
@@ -32,11 +35,15 @@ Future<void> init() async {
       () => PokemonRemoteDataSourceImpl(
             client: sl(),
           ));
+  sl.registerLazySingleton<PokemonLocalDataSource>(
+      () => PokemonLocalDataSourceImpl());
 
   //! Core
   sl.registerLazySingleton<NetworkInfo>(() => NetworkInfoImpl(sl()));
 
   //! External
+  await Hive.initFlutter();
+  final pokedexEntriesBox = await Hive.openBox('pokedex');
   sl.registerLazySingleton(() => http.Client());
   sl.registerLazySingleton(() => DataConnectionChecker());
 }
