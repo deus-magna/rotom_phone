0fae106198bfba3eb1bee638e73e7a1f7e2a4320
Renato <52421382+deus-magna@users.noreply.github.com>
2021-06-01 16:24:26 -0600

    Se quita el linter de documentacion temporalmente

--- lib/core/framework/decorations.dart
+++ lib/core/framework/decorations.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 var boxSahdow = [
   BoxShadow(
     color: Colors.black.withOpacity(0.16),
-    offset: Offset(0, 3),
+    offset: const Offset(0, 3),
     blurRadius: 6.0,
   )
 ];
--- lib/core/framework/painters/pokedex_header_clipper.dart
+++ lib/core/framework/painters/pokedex_header_clipper.dart
@@ -3,50 +3,51 @@ import 'package:flutter/material.dart';
 class PokedexHeaderClipper extends CustomClipper<Path> {
   @override
   Path getClip(Size size) {
-    Path path = Path();
-    final double _xScaling = size.width / 375;
-    final double _yScaling = size.height / 230;
-    path.lineTo(0 * _xScaling, 215.219 * _yScaling);
-    path.cubicTo(
-      62.01390959830233 * _xScaling,
-      225.06990287368183 * _yScaling,
-      124.70855853487637 * _xScaling,
-      230.01224030551165 * _yScaling,
-      187.5 * _xScaling,
-      230 * _yScaling,
-    );
-    path.cubicTo(
-      250.29144146512363 * _xScaling,
-      230.01224030551163 * _yScaling,
-      312.98609040169765 * _xScaling,
-      225.06990287368183 * _yScaling,
-      375 * _xScaling,
-      215.219 * _yScaling,
-    );
-    path.cubicTo(
-      375 * _xScaling,
-      215.219 * _yScaling,
-      375 * _xScaling,
-      0 * _yScaling,
-      375 * _xScaling,
-      0 * _yScaling,
-    );
-    path.cubicTo(
-      375 * _xScaling,
-      0 * _yScaling,
-      0 * _xScaling,
-      0 * _yScaling,
-      0 * _xScaling,
-      0 * _yScaling,
-    );
-    path.cubicTo(
-      0 * _xScaling,
-      0 * _yScaling,
-      0 * _xScaling,
-      215.219 * _yScaling,
-      0 * _xScaling,
-      215.219 * _yScaling,
-    );
+    final path = Path();
+    final _xScaling = size.width / 375;
+    final _yScaling = size.height / 230;
+    path
+      ..lineTo(0 * _xScaling, 215.219 * _yScaling)
+      ..cubicTo(
+        62.01390959830233 * _xScaling,
+        225.06990287368183 * _yScaling,
+        124.70855853487637 * _xScaling,
+        230.01224030551165 * _yScaling,
+        187.5 * _xScaling,
+        230 * _yScaling,
+      )
+      ..cubicTo(
+        250.29144146512363 * _xScaling,
+        230.01224030551163 * _yScaling,
+        312.98609040169765 * _xScaling,
+        225.06990287368183 * _yScaling,
+        375 * _xScaling,
+        215.219 * _yScaling,
+      )
+      ..cubicTo(
+        375 * _xScaling,
+        215.219 * _yScaling,
+        375 * _xScaling,
+        0 * _yScaling,
+        375 * _xScaling,
+        0 * _yScaling,
+      )
+      ..cubicTo(
+        375 * _xScaling,
+        0 * _yScaling,
+        0 * _xScaling,
+        0 * _yScaling,
+        0 * _xScaling,
+        0 * _yScaling,
+      )
+      ..cubicTo(
+        0 * _xScaling,
+        0 * _yScaling,
+        0 * _xScaling,
+        215.219 * _yScaling,
+        0 * _xScaling,
+        215.219 * _yScaling,
+      );
     return path;
   }
 
